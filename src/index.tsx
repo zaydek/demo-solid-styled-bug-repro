0dd0dbeb43be2413d7de21525278858259b9157b
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2022-10-01 20:56:59 -0700

    Search

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import "the-new-css-reset"
 import "virtual:uno.css"
 import "./scss/index.scss"
 
-import { createResource, createRoot, createSignal, For, Show, Suspense } from "solid-js"
+import { createMemo, createResource, createRoot, createSignal, For, Show, Suspense } from "solid-js"
 import { Dynamic, Portal, render } from "solid-js/web"
 import { serialize } from "./serialize"
 import { css } from "./solid-utils"
@@ -15,7 +15,7 @@ async function cache<Value>(key: string, value: Value): Promise<Value> {
 	if (_internalCache.has(key)) {
 		return _internalCache.get(key) as Value
 	} else {
-		//// await new Promise(resolve => setTimeout(resolve, 500))
+		await new Promise(resolve => setTimeout(resolve, 1_000))
 		_internalCache.set(key, value)
 		return value
 	}
@@ -78,19 +78,74 @@ const ready = () => manifest.state === "ready" && icons.state === "ready"
 
 ////////////////////////////////////////
 
-function SearchBar() {
-	const [value, setValue] = createSignal("")
+const [searchValue, setSearchValue] = createSignal("")
 
+// Memo because of <Highlight>
+const canonicalSearchValue = createMemo(() => {
+	return searchValue()
+		.trim()
+		.replace(/\s+/g, "-")           // Whitespace -> hyphen (takes precedence)
+		.replace(/-+/g, "-")            // Hyphens    -> hyphen
+		.replace(/[^a-zA-Z0-9-]+/g, "") // Remove non-alphanums
+})
+
+const payload = () => manifest()?.manifest.payload
+const payloadValues = () => {
+	if (!payload()) { return }
+	return Object.values(payload()!)
+}
+
+
+type IndexedResult = {
+	kebab: string
+	camel: string
+	title: string
+} & { index?: number }
+
+const searchResults = createRoot(() => {
+	return (): undefined | IndexedResult[] => {
+		if (!manifest()) { return } // Undefined
+
+		const value = canonicalSearchValue()
+		if (!value) {
+			return payload()
+		}
+		const matches: IndexedResult[] = []
+		for (const info of payloadValues()!) {
+			const key = info.kebab
+			const index = key.indexOf(value)
+			if (index !== -1) {
+				matches.push({
+					...info,
+					index,
+				})
+			}
+		}
+		if (matches.length) {
+			matches.sort((a, b) => a.index! - b.index!)
+			return matches
+		} else {
+			return // Undefined
+		}
+	}
+})
+
+//// createRoot(() => {
+//// 	createEffect(() => {
+//// 		if (!searchResults()) { return }
+//// 		console.log(searchResults())
+//// 	})
+//// })
+
+////////////////////////////////////////
+
+function SearchBar() {
 	return <>
 		{css`
 			.component-search-bar {
 				padding: 0 calc((var(--grid-cell-height) * 2 / 3) / 2);
 				height: 64px;
-				border-radius: calc((var(--grid-cell-height) * 2 / 3) / 2);
-				// Decoration
-				box-shadow: 0 0 0 4px var(--hairline-gray-color);
-			}
-			.component-search-bar.is-loaded {
+				border-radius: calc((var(--grid-cell-height) * 2 / 3) / 3);
 				// Typography
 				font: 400 17px / normal var(--sans);
 				letter-spacing: calc(1em / 64);
@@ -108,7 +163,7 @@ function SearchBar() {
 				<div class="h-8px aspect-16 rounded-$full effect-shimmer"></div>
 			</div>
 		</>}>
-			<input class="component-search-bar is-loaded" placeholder="I am looking for…" value={value()} onInput={e => setValue(e.currentTarget.value)} />
+			<input class="component-search-bar" placeholder="I am looking for…" value={searchValue()} onInput={e => setSearchValue(e.currentTarget.value)} autofocus />
 		</Show>
 	</>
 }
@@ -117,44 +172,76 @@ function Grid() {
 	return <>
 		{css`
 			.component-grid {
+				border-radius: calc((var(--grid-cell-height) * 2 / 3) / 3);
+				box-shadow: 0 0 0 4px var(--hairline-gray-color);
+			}
+			.component-grid.is-empty {
+				min-height: 75vh;
+				//// display: grid;
+				//// place-items: center;
+			}
+			.component-grid.is-not-empty {
 				display: grid;
 				grid-template-columns: repeat(auto-fill, minmax(var(--grid-cell-height), 1fr));
 				gap: 1px;
-				border-radius: calc((var(--grid-cell-height) * 2 / 3) / 2);
-				// Decoration
-				box-shadow: 0 0 0 4px var(--hairline-gray-color);
-				//// // Behavior
-				//// overflow: hidden;
+				//// border-radius: calc((var(--grid-cell-height) * 2 / 3) / 3);
+				//// // Decoration
+				//// box-shadow: 0 0 0 4px var(--hairline-gray-color);
+				//// //// // Behavior
+				//// //// overflow: hidden;
 			}
 
 			//////////////////////////////////
 
 			.component-grid-cell {
-				padding: calc(var(--grid-cell-height) / 8) calc(var(--grid-cell-height) / 16);
+				padding: calc(var(--grid-cell-height) / 8) calc(var(--grid-cell-height) / 32);
 				display: grid;               // E.g. grid
 				grid-template-rows: 1fr 6px;
 				place-items: center;         // E.g. grid grid-center
 				gap: 6px;
 				aspect-ratio: 1;
 				//// outline: 1px solid var(--hairline-gray-color);
+				// Behavior
+				cursor: pointer;
 			}
 
 			//////////////////////////////////
 
 			.component-grid-label {
+				// Typography
 				font: 400 12.5px / normal var(--sans);
-				letter-spacing: calc(1em / 64);
-				color: var(--text-color);
-				// Ellipsis
+				// Ellipsis (typography)
 				overflow: hidden; // Sorted alphabetically
 				text-align: center;
 				text-overflow: ellipsis;
 				white-space: nowrap;
 				width: 100%;
+				//// letter-spacing: calc(1em / 64);
+				color: var(--text-color);
+				// Behavior
+				cursor: text;
+				user-select: all;
+				-webkit-user-select: all;
+			}
+
+			//////////////////////////////////
+
+			//// .component-grid-label-highlight {
+			//// 	display: inline-block; // CSS reset
+			//// }
+
+			.component-grid-label-highlight { position: relative; }
+			.component-grid-label-highlight::before { content: ""; }
+			.component-grid-label-highlight::before {
+				position: absolute;
+				z-index: -10;
+				inset: 0;
+				border-radius: 2px;
+				background-color: hsl(200deg 100% 90%);
 			}
 		`}
 		<Suspense fallback={<>
-			<div class="component-grid">
+			<div class="component-grid is-not-empty">
 				<For each={range(64)}>
 					{() => <>
 						<div class="component-grid-cell">
@@ -165,21 +252,41 @@ function Grid() {
 				</For>
 			</div>
 		</>}>
-			<div class="component-grid">
-				<For each={manifest()?.manifest.payload}>
-					{info => <>
-						<button class="component-grid-cell" onClick={e => {
-							const serialized = serialize(e.currentTarget.querySelector("svg")!)
-							navigator.clipboard.writeText(serialized)
-							setTextarea(serialized)
-						}}>
-							{/* @ts-expect-error */}
-							<Dynamic component={icons()?.[info.title]} class="h-32px aspect-1 color:$text-color" />
-							<div class="component-grid-label">{info.kebab}</div>
-						</button>
-					</>}
-				</For>
-			</div>
+			<Show when={searchResults()} fallback={<>
+				<div class="component-grid is-empty flex-col flex-center gap-8px">
+					<div class="font-family:$sans color:$text-color">No results</div>
+					<div class="px-24px h-48px rounded-$full background-color:hsl(0deg_0%_0%/15%) grid grid-center cursor:pointer" onClick={e => {
+						setSearchValue("")
+						const componentSearchBar = document.getElementsByClassName("component-search-bar")[0]! as HTMLElement
+						componentSearchBar.focus()
+					}} tabindex="0">
+						<div class="font-family:$sans color:$text-color">Search for something else</div>
+					</div>
+				</div>
+			</>}>
+				<div class="component-grid is-not-empty">
+					<For each={searchResults()}>
+						{info => <>
+							<div class="component-grid-cell" onClick={e => {
+								const serialized = serialize(e.currentTarget.querySelector("svg")!)
+								navigator.clipboard.writeText(serialized)
+								setTextarea(serialized)
+							}}>
+								{/* @ts-expect-error */}
+								<Dynamic component={icons()?.[info.title]} class="h-32px aspect-1 color:$text-color" />
+								<div class="component-grid-label">
+									{/* Guard undefined because info.index=0 is OK */}
+									<Show when={info.index !== undefined} fallback={info.kebab}>
+										<span data-a>{info.kebab.slice(0, info.index!)}</span>
+										<span data-b class="component-grid-label-highlight">{canonicalSearchValue()}</span>
+										<span data-c>{info.kebab.slice(info.index! + canonicalSearchValue().length)}</span>
+									</Show>
+								</div>
+							</div>
+						</>}
+					</For>
+				</div>
+			</Show>
 		</Suspense>
 	</>
 }
@@ -224,6 +331,10 @@ function Modal() {
 						// Decoration
 						color: var(--text-color);
 						background-color: hsl(0deg 0% 95%);
+						//// // Behavior
+						//// cursor: text;
+						//// user-select: all;
+						//// -webkit-user-select: all;
 					}
 					.textarea::placeholder {
 						color: var(--placeholder-text-color);
@@ -231,32 +342,36 @@ function Modal() {
 				`}
 				<For each={["v1", "v2"] as Version[]}>
 					{version => <>
-						<button class="button" onClick={e => setVersion(version)}>
+						<div class="button" onClick={e => setVersion(version)} tabindex="0">
 							{version}
-						</button>
+						</div>
 					</>}
 				</For>
 				<hr class="hr" />
 				<Show when={version() === "v1"}>
 					<For each={["solid", "outline"] as Variant[]}>
 						{variant => <>
-							<button class="button" onClick={e => setVariant(variant)}>
+							<div class="button" onClick={e => setVariant(variant)} tabindex="0">
 								{variant}
-							</button>
+							</div>
 						</>}
 					</For>
 				</Show>
 				<Show when={version() === "v2"}>
 					<For each={["20/solid", "24/solid", "24/outline"] as Variant[]}>
 						{variant => <>
-							<button class="button" onClick={e => setVariant(variant)}>
+							<div class="button" onClick={e => setVariant(variant)} tabindex="0">
 								{variant}
-							</button>
+							</div>
 						</>}
 					</For>
 				</Show>
 				<hr class="hr" />
-				<textarea class="textarea" rows={textarea().split("\n").length} value={textarea()} placeholder="Click an element to inspect the HTML" />
+				<textarea class="textarea" rows={textarea().split("\n").length} value={textarea()} placeholder="Click an element to inspect the HTML" onClick={e => {
+					// https://stackoverflow.com/a/7313795
+					e.currentTarget.focus()
+					e.currentTarget.select()
+				 }} />
 			</div>
 		</div>
 	</>
