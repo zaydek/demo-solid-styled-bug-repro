614cb6fd297457a934543483408a5797900626cc
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2022-10-15 20:44:17 -0700

    Cleaned up CSS reset

--- src/index.tsx
+++ src/index.tsx
@@ -8,19 +8,21 @@ import { createSignal, ParentProps, Setter } from "solid-js"
 import { For, render, Show } from "solid-js/web"
 import { Bottomsheet, Sidesheet, SidesheetState } from "solid-sheet"
 import { createMediaQuery } from "./effects"
+import { css } from "./solid-utils"
 import { only, range } from "./utils"
 
 ////////////////////////////////////////
+
+const responsive = createMediaQuery("(max-width: 499px)")
 
 function Sheet(props: ParentProps<{
 	sidesheet:    SidesheetState
 	setSidesheet: Setter<SidesheetState>
 }>) {
-	const responsive = createMediaQuery("(max-width: 500px)")
 
 	return <>
 		<Show when={responsive()}>
-			<Bottomsheet initialState="closed">
+			<Bottomsheet initialState="open">
 				{props.children}
 			</Bottomsheet>
 		</Show>
@@ -35,20 +37,86 @@ function Sheet(props: ParentProps<{
 ////////////////////////////////////////
 
 function App() {
-	const [sidesheet, setSidesheet] = createSignal<SidesheetState>("closed")
+	const [sidesheet, setSidesheet] = createSignal<SidesheetState>("open")
 
 	return <>
+		{css`
+			:root {
+				--fixed-navbar-height: calc(32px + 16px + 16px);
+			}
+			.fixed-navbar {
+				position: fixed;
+				z-index: 10;
+				inset: 0 0 auto 0;
+				padding: 16px;
+				background-color: white;
+				box-shadow: 0 0 0 4px hsl(0 0% 0% / 25%);
+
+				/* Flow */
+				display: flex;
+				flex-direction: row;
+				align-items: center; /* Center y-axis */
+				gap: 16px;
+			}
+			.navbar {
+				padding: 16px;
+				/* Defer background-color and box-shadow to .*-card */
+
+				/* Flow */
+				display: flex;
+				flex-direction: row;
+				align-items: center; /* Center y-axis */
+				gap: 16px;
+			}
+			.nav-icon {
+				height: 32px;
+				aspect-ratio: 1;
+				border-radius: 1000px;
+				background-color: gray;
+			}
+			.line { height: 1px; background-color: hsl(0 0% 90%); }
+			.line.is-collapsed { margin-top: -1px; }
+		`}
+		{/* @ts-expect-error */}
+		<nav class="fixed-navbar" inert={only(sidesheet() === "expanded")}>
+			<div class="nav-icon"></div>
+			<div class="nav-icon"></div>
+			<div class="[flex-grow:1]"></div>
+			<div class="nav-icon"></div>
+		</nav>
 		{/* @ts-expect-error */}
 		<main class="main-content" inert={only(sidesheet() === "expanded")}>
 			<For each={range(1000)}>{() => <>
-				Hello, world!
+				x{" "}
 			</>}</For>
 		</main>
 		<Sheet sidesheet={sidesheet()} setSidesheet={setSidesheet}>
 			<aside class="aside-content">
-				<For each={range(1000)}>{() => <>
-					Hello, world!
-				</>}</For>
+				<Show when={!responsive()}>
+					<div class="[flex-shrink:0]">
+						<nav class="navbar">
+							<div class="nav-icon"></div>
+							<div class="[flex-grow:1]"></div>
+							<div class="nav-icon"></div>
+							<div class="nav-icon"></div>
+						</nav>
+						<hr class="line" />
+					</div>
+				</Show>
+				<div class="[flex-grow:1] [overflow-y:auto]">
+					<For each={range(40)}>{() => <>
+						<section class="[padding:16px]">
+							<div>Hello, world!</div>
+						</section>
+						<hr class="line" />
+					</>}</For>
+				</div>
+				<div class="[flex-shrink:0]">
+					<hr class="line is-collapsed" />
+					<section class="[padding:16px]">
+						<div>Hello, world!</div>
+					</section>
+				</div>
 			</aside>
 		</Sheet>
 	</>
