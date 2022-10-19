4800a1904023824778a49332747419f0b656c255
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2022-10-18 20:30:46 -0700

    Panel -> drawer

---
+++ src/drawer.tsx
@@ -0,0 +1,336 @@
+import { Accessor, batch, createContext, createEffect, createSignal, DEV, JSX, onMount, ParentProps, Show, untrack, useContext } from "solid-js"
+import { createStore } from "solid-js/store"
+import { Portal } from "solid-js/web"
+import { e } from "unocss"
+import { css, cx } from "./solid-utils"
+import { stringify } from "./utils"
+
+type Element = {
+	height:     number
+	open:       boolean
+	transition: boolean
+	computed: {
+		translateY: number
+		clipHeight: number
+	}
+}
+
+type State = {
+	collapseHeight: Accessor<number>
+	elements:       Element[]
+	boundingBox:    Accessor<undefined | number>
+	maskTransition: Accessor<boolean>
+}
+
+type Actions = {
+	createElement: (_: { height: number, open: boolean }) => number
+  open:          (index: number) => void
+  close:         (index: number) => void
+  toggle:        (index: number) => void
+  transitionend: (index: number) => void
+}
+
+export const DrawerContext = createContext<{
+	state:   State
+	actions: Actions
+}>()
+
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+
+export function Drawer(props: ParentProps<{
+	open?: boolean
+
+	head: JSX.Element
+}>) {
+	const { state, actions } = useContext(DrawerContext)!
+
+	const [ref, setRef] = createSignal<HTMLElement>()
+	const [index, setIndex] = createSignal<number>()
+
+	const element = () => {
+		if (index() === undefined) { return }
+		return state.elements[index()!]
+	}
+
+	onMount(() => {
+		const height = ref()!.clientHeight
+		const open = props.open ?? false
+		setIndex(actions.createElement({ height, open }))
+	})
+
+	return <>
+		<div
+			ref={setRef}
+			class={element() === undefined
+				? "drawer"
+				: cx(`drawer ${element()!.open ? "is-open" : "is-closed"}`)
+			}
+			style={{
+				...(element() && {
+					// DEBUG
+					"background-color": `hsl(${index()! * 60} 100% 75%)`,
+
+					...(!element()!.transition && {
+						"height": `${element()!.computed.clipHeight}px`,
+						"overflow-y": "clip",
+					}),
+					"transform": `translateY(${element()!.computed.translateY}px)`,
+				}),
+			}}
+			onTransitionEnd={e => actions.transitionend(index()!)}
+		>
+			<div
+				class="drawer-head"
+				onClick={e => actions.toggle(index()!)}
+				onKeyDown={e => {
+					if (e.key === " ") {
+						e.preventDefault()
+						e.currentTarget.click()
+					}
+				}}
+				tabIndex={0}
+			>
+				{props.head}
+			</div>
+			<div class="drawer-body">
+				{props.children}
+			</div>
+		</div>
+	</>
+}
+
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+
+function DEBUG_VIEW() {
+	const { state } = useContext(DrawerContext)!
+
+	return <>
+		{css`
+			.drawer-debug-card {
+				position: fixed;
+				z-index: 10;
+				inset: auto auto 16px 16px;
+				/* LAYOUT */
+				padding: 16px;
+				max-height: calc(var(--screen-y) - 16px * 2);
+				overflow-y: auto;
+				width: 320px;
+				border-radius: 16px;
+				/* DECORATION */
+				background-color: white;
+				box-shadow: 0 0 0 4px hsl(0 0% 0% / 25%);
+			}
+			.drawer-debug-type {
+				font: 400 12px / 1.25 Monaco;
+				white-space: pre;
+			}
+		`}
+		<Portal ref={el => el.className = "portal"}>
+			<div class="debug-drawer">
+				<div class="debug-drawer-type">
+					{stringify(state, 2)}
+				</div>
+			</div>
+		</Portal>
+	</>
+}
+
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+////////////////////////////////////////
+
+export function DrawerProvider(props: ParentProps<{ collapseHeight: number }>) {
+	const collapseHeight = () => props.collapseHeight
+
+	const [elements, setElements] = createStore<Element[]>([])
+	const [boundingBox, setBoundingBox] = createSignal<number>()
+	const [maskTransition, setMaskTransition] = createSignal(false)
+
+	function createElement({ height, open }: { height: number, open: boolean }) {
+		const index = elements.length
+		setElements(curr => [
+			...curr, {
+				height,
+				open,
+				transition: false,
+				computed: {
+					translateY: 0, // Defer initialization; TODO: Shouldn’t this be undefined?
+					clipHeight: 0, // Defer initialization; TODO: Shouldn’t this be undefined?
+				},
+			},
+		])
+		return index
+	}
+
+	function open(index: number) {
+		batch(() => {
+			setElements(index, curr => ({
+				...curr,
+				open: true,
+				transition: true,
+			}))
+			setMaskTransition(true)
+		})
+	}
+
+	function close(index: number) {
+		batch(() => {
+			setElements(index, curr => ({
+				...curr,
+				open: false,
+				transition: true,
+			}))
+			setMaskTransition(true)
+		})
+	}
+
+	function transitionend(index: number) {
+		setElements(index, "transition", false)
+	}
+
+	function toggle(index: number) {
+		if (elements[index].open) {
+			close(index)
+		} else {
+			open(index)
+		}
+	}
+
+	createEffect(() => {
+		if (!elements.length) { return }
+
+		batch(() => {
+			let _boundingBox = 0
+			for (let index = 0; index < elements.length; index++) {
+				const element = elements[index]
+				setElements(index, "computed", "translateY", _boundingBox)
+				_boundingBox += element.open
+					? element.height
+					: collapseHeight()
+			}
+			// Now that we know the bounding box for all elements, compute the
+			// clipHeight per element
+			for (let index = 0; index < elements.length; index++) {
+				setElements(index, "computed", curr => ({
+					...curr,
+					clipHeight: _boundingBox - curr.translateY,
+				}))
+			}
+			if (untrack(boundingBox) === undefined) { // Initialize
+				setBoundingBox(_boundingBox)
+			} else {
+				if (_boundingBox > untrack(boundingBox)!) {
+					setBoundingBox(_boundingBox) // Synchronous
+				} else {
+					createEffect(() => { // Asynchronous
+						if (maskTransition()) { return }
+						setBoundingBox(_boundingBox)
+					})
+				}
+			}
+		})
+	})
+
+	return <>
+		<DrawerContext.Provider value={{
+			state: { collapseHeight, elements, boundingBox,  maskTransition },
+			actions: { createElement, open, close, toggle, transitionend },
+		}}>
+			{css`
+				.drawer-container {
+					position: relative;
+				}
+
+				/******************************/
+
+				.drawer {
+					position: absolute;
+					inset:
+						auto /* T */
+						0    /* R */
+						auto /* B */
+						0;   /* L */
+				}
+
+				/******************************/
+
+				.drawer-end {
+					position: absolute;
+					z-index: 50;
+					inset: 0 0 auto 0;
+					height: 500px;
+					background-color: purple;
+				}
+				:root:has(body.ready) .drawer-end {
+					transition: transform 1000ms cubic-bezier(0, 1, 0.25, 1);
+				}
+
+				/******************************/
+
+				.drawer {
+					cursor: pointer;
+				}
+				:root:has(body.ready) .drawer {
+					transition: transform 1000ms cubic-bezier(0, 1, 0.25, 1);
+				}
+
+				/******************************/
+
+				.drawer-head {
+					height: 32px;
+				}
+
+				/******************************/
+
+				:root:has(body.ready) .drawer-body {
+					transition: opacity 1000ms cubic-bezier(0, 1, 0.25, 1);
+				}
+				.drawer.is-closed .drawer-body { opacity: 0; }
+				.drawer.is-open   .drawer-body { opacity: 1; }
+			`}
+			<div
+				class="drawer-container"
+				style={{
+					...((boundingBox() !== undefined && maskTransition() !== undefined) && {
+						"height": `${boundingBox()!}px`,
+						"overflow-y": maskTransition()
+							? "clip"
+							: "auto",
+					}),
+					//// // DEBUG
+					//// "outline": "8px solid red",
+				}}
+			>
+				{props.children}
+				<Show when={elements.length > 0}>
+					<div
+						class="drawer-end"
+						style={{
+							"height": maskTransition()
+								? `${boundingBox()}px` // Lazy but works
+								: "0px",
+							"transform": `translateY(${elements[elements.length - 1].computed.translateY + (
+								elements[elements.length - 1].open
+									? elements[elements.length - 1].height
+									: collapseHeight()
+							)}px)`,
+						}}
+						onTransitionEnd={e => setMaskTransition(false)}
+					></div>
+				</Show>
+			</div>
+
+			{/* DEBUG */}
+			<Show when={DEV}>
+				<DEBUG_VIEW />
+			</Show>
+		</DrawerContext.Provider>
+	</>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,13 @@ import "./2-vars.css"
 import "./3-components.css"
 import "./uno.generated.css"
 
-import { Accessor, batch, createContext, createEffect, createRoot, createSignal, onMount, ParentProps, Setter, untrack, useContext } from "solid-js"
-import { createStore } from "solid-js/store"
+import { batch, createRoot, createSignal, onMount, ParentProps, Setter } from "solid-js"
 import { For, render, Show } from "solid-js/web"
 import { Bottomsheet, Sidesheet, SidesheetState } from "solid-sheet"
+import { Drawer, DrawerProvider } from "./drawer"
 import { createMediaQuery } from "./effects"
 import { css, cx } from "./solid-utils"
-import { only, range, stringify } from "./utils"
+import { only, range } from "./utils"
 
 ////////////////////////////////////////
 
@@ -111,57 +111,57 @@ function App() {
 						</section>
 						<hr class="line" />
 					</>}</For> */}
-					<PanelProvider collapseHeight={32}>
-						<Panel>
+					<DrawerProvider collapseHeight={32}>
+						<Drawer head={<>Hello, world!</>}>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-						<Panel>
-							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+						</Drawer>
+						<Drawer head={<>Hello, world!</>}>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-						<Panel>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-						<Panel>
+							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+						</Drawer>
+						<Drawer head={<>Hello, world!</>}>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-						<Panel>
+						</Drawer>
+						<Drawer head={<>Hello, world!</>}>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-						<Panel>
+						</Drawer>
+						<Drawer head={<>Hello, world!</>}>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-						<Panel>
+						</Drawer>
+						<Drawer head={<>Hello, world!</>}>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						</Panel>
-					</PanelProvider>
+						</Drawer>
+						<Drawer head={<>Hello, world!</>}>
+							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+							<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+						</Drawer>
+					</DrawerProvider>
 				</div>
 				<div class="[flex-shrink:0]">
 					<hr class="line is-collapsed" />
@@ -308,309 +308,6 @@ createRoot(() => {
 	}))
 })
 
-type Element = {
-	height:     number
-	open:       boolean
-	transition: boolean
-	computed: {
-		translateY: number
-		clipHeight: number
-	}
-}
-
-type State = {
-	collapseHeight: Accessor<number>
-	elements:       Element[]
-	boundingBox:    Accessor<undefined | number>
-	someTransition: Accessor<boolean>
-}
-
-type Actions = {
-	createElement: (_: { height: number, open: boolean }) => number
-  open:          (index: number) => void
-  close:         (index: number) => void
-  toggle:        (index: number) => void
-  transitionend: (index: number) => void
-}
-
-export const PanelContext = createContext<{
-	state:   State
-	actions: Actions
-}>()
-
-function PanelProvider(props: ParentProps<{ collapseHeight: number }>) {
-	const collapseHeight = () => props.collapseHeight
-
-	const [elements, setElements] = createStore<Element[]>([])
-	const [boundingBox, setBoundingBox] = createSignal<number>()
-	const [someTransition, setSomeTransition] = createSignal(false)
-
-	function createElement({ height, open }: { height: number, open: boolean }) {
-		const index = elements.length
-		setElements(curr => [
-			...curr, {
-				height,
-				open,
-				transition: false,
-				computed: {
-					translateY: 0, // Defer
-					clipHeight: 0, // Defer
-				},
-			},
-		])
-		return index
-	}
-
-	function open(index: number) {
-		batch(() => {
-			//// const atEnd = index === elements.length - 1
-			setElements(index, curr => ({
-				...curr,
-				open: true,
-				transition: true, //// !atEnd,
-			}))
-			setSomeTransition(true)
-		})
-	}
-
-	function close(index: number) {
-		batch(() => {
-			//// const atEnd = index === elements.length - 1
-			setElements(index, curr => ({
-				...curr,
-				open: false,
-				transition: true, //// !atEnd,
-			}))
-			setSomeTransition(true)
-		})
-	}
-
-	function transitionend(index: number) {
-		setElements(index, "transition", false)
-	}
-
-	function toggle(index: number) {
-		if (elements[index].open) {
-			close(index)
-		} else {
-			open(index)
-		}
-	}
-
-	// Synchronize computed.transition and boundingBox
-	createEffect(() => {
-		if (!elements.length) { return }
-
-		batch(() => {
-			let _boundingBox = 0
-			for (let index = 0; index < elements.length; index++) {
-				const element = elements[index]
-				setElements(index, "computed", "translateY", _boundingBox)
-				_boundingBox += element.open
-					? element.height
-					: collapseHeight()
-			}
-			// Now that we know bounding box, we can compute clipHeight
-			for (let index = 0; index < elements.length; index++) {
-				setElements(index, "computed", curr => ({
-					...curr,
-					clipHeight: _boundingBox - curr.translateY,
-				}))
-			}
-			//// setBoundingBox(_boundingBox)
-			if (untrack(boundingBox) === undefined) { // Initialize
-				setBoundingBox(_boundingBox)
-			} else {
-				if (_boundingBox > untrack(boundingBox)!) {
-					setBoundingBox(_boundingBox) // Synchronous
-				} else {
-					createEffect(() => { // Asynchronous
-						if (someTransition()) { return }
-						setBoundingBox(_boundingBox)
-					})
-				}
-			}
-		})
-	})
-
-	return <>
-		{css`
-			.debug-panel {
-				position: fixed;
-				z-index: 10;
-				inset: auto auto 16px 16px;
-				/* LAYOUT */
-				padding: 16px;
-				max-height: calc(var(--screen-y) - 16px * 2);
-				overflow-y: auto;
-				width: 320px;
-				border-radius: 16px;
-				/* DECORATION */
-				background-color: white;
-				box-shadow: 0 0 0 4px hsl(0 0% 0% / 25%);
-			}
-			.debug-panel-typography {
-				font: 400 12px / 1.25 Monaco;
-				white-space: pre;
-			}
-		`}
-		{/* DEBUG */}
-		{/* <Show when={window.innerWidth >= 500}>
-			<div class="debug-panel">
-				<div class="debug-panel-typography">
-					{stringify({ collapseHeight, elements, boundingBox, someTransition }, 2)}
-				</div>
-			</div>
-		</Show> */}
-		<PanelContext.Provider value={{
-			state: { collapseHeight, elements, boundingBox, someTransition },
-			actions: { createElement, open, close, toggle, transitionend },
-		}}>
-			{css`
-				.panel-container {
-					position: relative;
-				}
-
-				/******************************/
-
-				.panel {
-					position: absolute;
-					inset:
-						auto /* T */
-						0    /* R */
-						auto /* B */
-						0;   /* L */
-				}
-
-				/******************************/
-
-				.panel-end {
-					position: absolute;
-					z-index: 50;
-					inset: 0 0 auto 0;
-					height: 500px;
-					background-color: purple;
-				}
-				:root:has(body.ready) .panel-end {
-					transition: transform 500ms cubic-bezier(0, 1, 0.25, 1);
-				}
-
-				/******************************/
-
-				.panel {
-					cursor: pointer;
-				}
-				:root:has(body.ready) .panel {
-					transition: transform 500ms cubic-bezier(0, 1, 0.25, 1);
-				}
-
-				/******************************/
-
-				.panel-head {
-					height: 32px;
-				}
-
-				/******************************/
-
-				:root:has(body.ready) .panel-body {
-					transition: opacity 500ms cubic-bezier(0, 1, 0.25, 1);
-				}
-				.panel.is-closed .panel-body { opacity: 0; }
-				.panel.is-open   .panel-body { opacity: 1; }
-			`}
-			<div
-				class="panel-container"
-				style={{
-					...((boundingBox() !== undefined && someTransition() !== undefined) && {
-						"height": `${boundingBox()!}px`,
-						"overflow-y": someTransition()
-							? "clip"
-							: "auto",
-					}),
-					//// // DEBUG
-					//// "outline": "8px solid red",
-				}}
-			>
-				{props.children}
-				<Show when={elements.length > 0}>
-					<div
-						class="panel-end"
-						style={{
-							"height": someTransition()
-								? `${boundingBox()}px` // Lazy but works
-								: "0px",
-							"transform": `translateY(${elements[elements.length - 1].computed.translateY + (
-								elements[elements.length - 1].open
-									? elements[elements.length - 1].height
-									: collapseHeight()
-							)}px)`,
-						}}
-						onTransitionEnd={e => setSomeTransition(false)}
-					></div>
-				</Show>
-			</div>
-		</PanelContext.Provider>
-	</>
-}
-
-function Panel(props: ParentProps<{ open?: boolean }>) {
-	const { state, actions } = useContext(PanelContext)!
-
-	const [ref, setRef] = createSignal<HTMLElement>()
-	const [index, setIndex] = createSignal<number>()
-
-	const element = () => {
-		if (index() === undefined) { return }
-		return state.elements[index()!]
-	}
-
-	onMount(() => {
-		const height = ref()!.clientHeight
-		const open = props.open ?? false
-		setIndex(actions.createElement({ height, open }))
-	})
-
-	return <>
-		<div
-			ref={setRef}
-			class={element() === undefined
-				? "panel"
-				: cx(`panel ${element()!.open ? "is-open" : "is-closed"}`)
-			}
-			style={{
-				...(element() && {
-					// DEBUG
-					"background-color": `hsl(${index()! * 60} 100% 75%)`,
-
-					...(!element()!.transition && {
-						"height": `${element()!.computed.clipHeight}px`,
-						"overflow-y": "clip",
-					}),
-					"transform": `translateY(${element()!.computed.translateY}px)`,
-				}),
-			}}
-			onTransitionEnd={e => actions.transitionend(index()!)}
-		>
-			<div
-				class="panel-head"
-				onClick={e => actions.toggle(index()!)}
-				onKeyDown={e => {
-					if (e.key === " ") {
-						e.preventDefault()
-						e.currentTarget.click()
-					}
-				}}
-				tabIndex={0}
-			>
-				Hello, world!
-			</div>
-			<div class="panel-body">
-				{props.children}
-			</div>
-		</div>
-	</>
-}
-
 function App2() {
 	return <>
 		{css`
@@ -629,200 +326,62 @@ function App2() {
 		`}
 		<div class="center">
 			<div class="sidebar [display:flex] [flex-direction:column]">
-				<PanelProvider collapseHeight={32}>
-					<Panel>
+				<DrawerProvider collapseHeight={32}>
+					<Drawer head={<>Hello, world!</>}>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-					<Panel>
-						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+					</Drawer>
+					<Drawer head={<>Hello, world!</>}>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-					<Panel>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-					<Panel>
+						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+					</Drawer>
+					<Drawer head={<>Hello, world!</>}>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-					<Panel>
+					</Drawer>
+					<Drawer head={<>Hello, world!</>}>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-					<Panel>
+					</Drawer>
+					<Drawer head={<>Hello, world!</>}>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-					<Panel>
+					</Drawer>
+					<Drawer head={<>Hello, world!</>}>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
 						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
-					</Panel>
-				</PanelProvider>
-			</div>
-		</div>
-	</>
-}
-
-// TODO: Add uncontrolled vs. controlled API
-function Collapsible2() {
-	const [ref, setRef] = createSignal<HTMLElement>()
-
-	const [state, setState] = createSignal<"closed" | "open">("open")
-	const [h1, setH1] = createSignal<number>()
-	const [h2, setH2] = createSignal<number>()
-	const [transition, setTransition] = createSignal<true>()
-
-	onMount(() => {
-		setH1(ref()!.firstElementChild!.scrollHeight)
-		setH2(ref()!.scrollHeight)
-	})
-
-	return <>
-		{css`
-			.collapsible {
-				/* Runtime values */
-				--__height-is-closed: 32px;
-				--__height-is-open: initial;
-			}
-			.collapsible.is-closed { height: var(--__height-is-closed); overflow-y: clip; }
-			.collapsible.is-open   { height: var(--__height-is-open);   overflow-y: clip; }
-			.collapsible.is-transition {
-				transition: height 500ms cubic-bezier(0, 1, 0.25, 1);
-			}
-
-			/********************************/
-
-			.collapsible-button:hover {
-				cursor: pointer;
-			}
-
-			/********************************/
-
-			.collapsible-content {
-				transition: opacity 500ms cubic-bezier(0, 1, 0.25, 1);
-			}
-			.collapsible.is-closed .collapsible-content { opacity: 0; }
-			.collapsible.is-open   .collapsible-content { opacity: 1; }
-
-			/* .collapsible-content { */
-			/* 	overflow-y: hidden; */
-			/* 	transition: transform 500ms cubic-bezier(0, 1, 0.25, 1); */
-			/* } */
-			/* .collapsible.is-closed .collapsible-content { transform: translateY(-100px); } */
-			/* .collapsible.is-open   .collapsible-content { transform: translateY(0px); } */
-		`}
-		<div
-			ref={setRef}
-			class={cx(`collapsible is-${state()} ${transition() ? "is-transition" : ""}`)}
-			style={{
-				"--__height-is-closed": !h1()
-					? "initial"
-					: `${h1()}px`,
-				"--__height-is-open": !h2()
-					? "initial"
-					: `${h2()}px`,
-			}}
-			onTransitionEnd={e => setTransition()}
-			// COMPAT/Firefox: Use tabIndex={-1} to prevent "overflow-y: *;" from
-			// being focusable
-			tabIndex={-1}
-		>
-			<div
-				class="collapsible-button [padding:16px]"
-				onClick={e => {
-					if (state() === "open") {
-						batch(() => {
-							setState("closed")
-							setTransition(true)
-						})
-					} else {
-						batch(() => {
-							setState("open")
-							setTransition(true)
-						})
-					}
-				}}
-				onKeyDown={e => {
-					if (e.key === " ") {
-						e.preventDefault() // Prevent scrolling
-						e.currentTarget.click()
-					}
-				}}
-				role="button"
-				tabIndex={0}
-			>
-				Title
-			</div>
-			{/* @ts-expect-error */}
-			<div class="collapsible-content [padding:16px] [padding-top:0px_!important]" inert={only(state() === "closed")}>
-				<div>🤔🤔🤔🤔🤔🤔</div>
-				<div>🤔🤔🤔🤔🤔🤔</div>
-				<div>🤔🤔🤔🤔🤔🤔</div>
-				<div>🤔🤔🤔🤔🤔🤔</div>
-				<div>🤔🤔🤔🤔🤔🤔</div>
-				<div>End</div>
-			</div>
-		</div>
-	</>
-}
-
-function App3() {
-	return <>
-		{css`
-			.container {
-				padding: 96px 0;
-
-				/* Flow */
-				display: flex;
-				flex-direction: column;
-				align-items: center;
-			}
-			.card {
-				width: 224px;
-				background-color: whitesmoke;
-				box-shadow: 0 0 0 4px hsl(0 0% 0% / 25%);
-			}
-			.line {
-				height: 1px;
-				background-color: red;
-			}
-		`}
-		<div class="container">
-			<div class="card">
-				<Collapsible2 />
-				<div class="line"></div>
-				<Collapsible2 />
-				<div class="line"></div>
-				<Collapsible2 />
-				<div class="line"></div>
-				<Collapsible2 />
+					</Drawer>
+					<Drawer head={<>Hello, world!</>}>
+						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+						<div class="[height:32px]">🤔🤔🤔🤔🤔🤔🤔</div>
+					</Drawer>
+				</DrawerProvider>
 			</div>
 		</div>
 	</>
 }
 
 render(() => <>
-	<App3 />
+	<App2 />
 </>, document.getElementById("root")!)
