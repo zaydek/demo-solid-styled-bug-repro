71e6700b1b3dd32213b8901cbc82c79390f45c16
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2022-10-22 05:35:28 -0700

    MVP highlight buttons

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import "./css"
 import { createSignal, For, ParentProps, VoidProps } from "solid-js"
 import { Dynamic, render } from "solid-js/web"
 import { SidesheetState } from "solid-sheet"
-import { AriaRadio, AriaRadiogroup, AriaSliderHorizontal, AriaSliderThumb } from "./aria"
+import { AriaButton, AriaCheckbox, AriaRadio, AriaRadiogroup, AriaSliderHorizontal, AriaSliderThumb } from "./aria"
 import { Drawer, DrawerProvider } from "./drawer"
 import { NonResponsive, Sheet } from "./sheet"
 import { SmileySVG } from "./smiley-svg"
-import { createScreenEffect, css, CSSProps } from "./utils/solid"
+import { createScreenEffect, css, CSSProps, RefProps } from "./utils/solid"
 import { only, range } from "./utils/vanilla"
 
 ////////////////////////////////////////
@@ -296,41 +296,169 @@ function App() {
 function App2() {
 	createScreenEffect()
 
+	const [checked1, setChecked1] = createSignal(false)
+	const [checked2, setChecked2] = createSignal(false)
+	const [checked3, setChecked3] = createSignal(false)
+
+	const [groupValue, setGroupValue] = createSignal("foo")
+
 	return <>
 		{css`
-			.center {
-				display: grid;
-				place-items: center;
-				height: var(--screen-y);
-			}
-			.button {
-				height: 48px;
-				aspect-ratio: 4;
+			.cp-highlight-button {
+				--color: var(--color);
+				height: 40px;
+				padding: 0 20px;
 				border-radius: 1000px;
-				background-color: gray;
+				/* Defer background-color and box-shadow */
+				/* Defer background-color and box-shadow */
 
 				/* Flow */
 				display: flex;
-				flex-direction: g
+				flex-direction: row;
+				justify-content: center; /* Center x-axis */
+				align-items: center;     /* Center y-axis */
+				gap: 10px;
 			}
-			.checkbox {
-				height: 28px;
+
+			/********************************/
+
+			.cp-highlight-button[aria-checked=false] {
+				background-color: white;
+				box-shadow: 0 0 0 1px hsl(0 0% 0% / 10%), 0 0 0 4px hsl(0 0% 0% / 10%);
+			}
+			.cp-highlight-button[aria-checked=true] {
+				background-color: var(--color);
+				box-shadow: /* 0 0 0 1px hsl(0 0% 0% / 10%), */ 0 0 0 4px hsl(0 0% 0% / 10%);
+			}
+
+			/********************************/
+
+			.cp-highlight-button-icon {
+				height: 20px;
+				aspect-ratio: 1;
+				/* Defer color */
+			}
+			.cp-highlight-button[aria-checked=false] { color: var(--color); }
+			.cp-highlight-button[aria-checked=true]  { color: white; }
+
+			/********************************/
+
+			.cp-highlight-button-label {
+				font: 600 14px / normal system-ui;
+				letter-spacing: 0.05em;
+				/* Defer color */
+			}
+			.cp-highlight-button[aria-checked=false] .cp-highlight-button-label { color: hsl(0 0% 25%); }
+			.cp-highlight-button[aria-checked=true]  .cp-highlight-button-label { color: white; }
+
+			/********************************/
+
+			.cp-highlight-button-checkbox {
+				position: relative;
+				height: 24px;
 				aspect-ratio: 1;
 				border-radius: calc(100% / 3);
 				background-color: white;
-				box-shadow: 0 0 0 4px hsl(0 0% 0% / 25%);
+				box-shadow: 0 0 0 1px hsl(0 0% 0% / 25%);
+
+				/* Flow */
+				display: grid;
+				place-items: center;
+			}
+			.cp-highlight-button[aria-checked=false] .cp-highlight-button-checkbox-check { display: none; }
+			.cp-highlight-button[aria-checked=true]  .cp-highlight-button-checkbox-check {
+				height: 60%;
+				aspect-ratio: 1;
+				color: var(--color);
+			}
+
+			/********************************/
+
+			.cp-highlight-button-radio {
+				position: relative;
+				height: 24px;
+				aspect-ratio: 1;
+				border-radius: 1000px;
+				background-color: white;
+				box-shadow: 0 0 0 1px hsl(0 0% 0% / 25%);
+
+				/* Flow */
+				display: grid;
+				place-items: center;
+			}
+			.cp-highlight-button[aria-checked=false] .cp-highlight-button-radio-check { display: none; }
+			.cp-highlight-button[aria-checked=true]  .cp-highlight-button-radio-check {
+				height: calc(100% / 3);
+				aspect-ratio: 1;
+				border-radius: 1000px;
+				background-color: var(--color);
 			}
 		`}
-		<div class="center">
-			<div class="button">
-				<div></div>
-				<div class="checkbox"></div>
+		<div class="[display:grid] [place-items:center] [height:$screen-y]">
+			<div class="[width:448px]">
+				<div class="[display:flex] [flex-direction:column] [gap:16px]">
+					<div class="[display:grid] [grid-template-columns:repeat(1,_1fr)] [gap:16px]">
+						<For each={[
+							//// { "--color": "#ffb13b", checked: checked1, setChecked: setChecked1, children: "SVG"   },
+							{ "--color": "hsl(200 100% 55%)", checked: checked2, setChecked: setChecked2, children: "INCLUDE MIT LICENSE" },
+							//// { "--color": "#4fc08d", checked: checked3, setChecked: setChecked3, children: "VUE"   },
+						]}>{props => <>
+								<AriaCheckbox class="cp-highlight-button" style={{ "--color": props["--color"] }} checked={props.checked()} setChecked={props.setChecked}>
+								<Dynamic component={SmileySVG} class="cp-highlight-button-icon" />
+								<div class="cp-highlight-button-label">{props.children}</div>
+								<div class="cp-highlight-button-checkbox">
+									<Dynamic
+										component={CheckSVG}
+										class="cp-highlight-button-checkbox-check"
+										style={{ "stroke-width": "6" }}
+									/>
+								</div>
+							</AriaCheckbox>
+						</>}</For>
+					</div>
+					<AriaRadiogroup class="[display:grid] [grid-template-columns:repeat(3,_1fr)] [gap:16px]" groupValue={groupValue()} setGroupValue={setGroupValue}>
+						<For each={[
+							{ "--color": "#ffb13b", value: "foo", children: "SVG"   },
+							{ "--color": "#61dafb", value: "bar", children: "REACT" },
+							{ "--color": "#4fc08d", value: "baz", children: "VUE"   },
+						]}>{props => <>
+							<AriaRadio class="cp-highlight-button" style={{ "--color": props["--color"] }} value={props.value}>
+								<Dynamic component={SmileySVG} class="cp-highlight-button-icon" />
+								<div class="cp-highlight-button-label">{props.children}</div>
+								<div class="cp-highlight-button-radio">
+									<div class="cp-highlight-button-radio-check"></div>
+								</div>
+							</AriaRadio>
+						</>}</For>
+					</AriaRadiogroup>
+				</div>
 			</div>
 		</div>
 	</>
 }
 
+function CheckSVG(props: VoidProps<RefProps & CSSProps>) {
+	return <>
+		<svg
+			// Base props
+			ref={el => props.ref?.(el as unknown as HTMLElement)}
+			class={props.class}
+			style={props.style}
+			// SVG props
+			fill="none"
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			stroke="currentColor"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<polyline points="20 6 9 17 4 12"></polyline>
+		</svg>
+	</>
+}
+
 render(() =>
-	<App />,
+	<App2 />,
 	document.getElementById("root")!,
 )
