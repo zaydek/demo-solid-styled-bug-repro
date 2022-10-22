7188a6c48af553a22766c45e4731071dfd04496d
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2022-10-22 08:22:44 -0700

    Remove reliance of flex-grow

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import "./css"
 
-import { createSignal, For, ParentProps, Show, VoidProps } from "solid-js"
+import { createSignal, For, JSX, ParentProps, Setter, Show, VoidProps } from "solid-js"
 import { Dynamic, render } from "solid-js/web"
 import { SidesheetState } from "solid-sheet"
 import { AriaCheckbox, AriaRadio, AriaRadiogroup, AriaSliderHorizontal, AriaSliderThumb } from "./aria"
@@ -12,6 +12,7 @@ import { only, range } from "./utils/vanilla"
 
 ////////////////////////////////////////
 
+// TODO: EXTRACT
 function NavIcon() {
 	return <>
 		<div class="cp-nav-icon">
@@ -22,13 +23,13 @@ function NavIcon() {
 
 ////////////////////////////////////////
 
+// TODO: EXTRACT
 function Radio(props: VoidProps<{ value: string }>) {
 	return <>
 		<AriaRadio class="cp-radio-container" value={props.value}>
 			<div class="cp-radio-label">
 				<Dynamic component={SmileySVG} class="cp-radio-label-svg" />
-				{/* TODO */}
-				<div class="[flex-grow:1]">Hello, world!</div>
+				<div>Hello, world!</div>
 			</div>
 			<div class="cp-radio">
 				<div class="cp-radio-check"></div>
@@ -37,6 +38,7 @@ function Radio(props: VoidProps<{ value: string }>) {
 	</>
 }
 
+// TODO: EXTRACT
 function Radiogroup(props: ParentProps<CSSProps>) {
 	const [groupValue, setGroupValue] = createSignal("foo")
 
@@ -44,6 +46,57 @@ function Radiogroup(props: ParentProps<CSSProps>) {
 		<AriaRadiogroup class={props.class} style={props.style} groupValue={groupValue()} setGroupValue={setGroupValue}>
 			{props.children}
 		</AriaRadiogroup>
+	</>
+}
+
+////////////////////////////////////////
+
+function CheckSVG(props: VoidProps<RefProps & CSSProps>) {
+	return <>
+		<svg ref={el => props.ref?.(el as unknown as HTMLElement)} class={props.class} style={props.style} fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+			<polyline points="20 6 9 17 4 12"></polyline>
+		</svg>
+	</>
+}
+
+// TODO: EXTRACT
+function CheckableCheckbox(props: ParentProps<{
+	checked:    boolean
+	setChecked: Setter<boolean>
+}>) {
+	return <>
+		<AriaCheckbox class="cp-checkable-container" checked={props.checked} setChecked={props.setChecked}>
+			<div class="cp-checkable-label">
+				<Dynamic component={SmileySVG} class="cp-checkable-label-icon" />
+				<div class="cp-checkable-label-text">
+					{props.children}
+				</div>
+			</div>
+			<div class="cp-checkable-checkbox">
+				<Dynamic component={CheckSVG} class="cp-checkable-checkbox-check" style={{ "stroke-width": "7" }} />
+			</div>
+		</AriaCheckbox>
+	</>
+}
+
+// TODO: EXTRACT
+function CheckableRadio(props: ParentProps<{
+	value: string
+
+	style: JSX.CSSProperties
+}>) {
+	return <>
+		<AriaRadio class="cp-checkable-container" style={props.style} value={props.value}>
+			<div class="cp-checkable-label">
+				<Dynamic component={SmileySVG} class="cp-checkable-label-icon" />
+				<div class="cp-checkable-label-text">
+					{props.children}
+				</div>
+			</div>
+			<div class="cp-checkable-radio">
+				<div class="cp-checkable-radio-check"></div>
+			</div>
+		</AriaRadio>
 	</>
 }
 
@@ -97,6 +150,7 @@ function App() {
 		{/* @ts-expect-error */}
 		<nav class="layout-nav" inert={only(sidesheet() === "expanded")}>
 			<NavIcon />
+			{/* TODO: Deprecate use of flex-grow */}
 			<div class="[flex-grow:1]">
 				<div>Hello, world!</div>
 			</div>
@@ -124,8 +178,8 @@ function App() {
 				grid-template-rows: 16px 1fr 32px;
 				place-items: center;
 			}
-			.results-grid-cell :nth-child(1) { grid-row: 2 / 3; }
-			.results-grid-cell :nth-child(2) { grid-row: 3 / 3; }
+			.results-grid-cell :nth-child(1) { grid-row: 2; }
+			.results-grid-cell :nth-child(2) { grid-row: 3; }
 			.results-grid-icon-svg {
 				height: 32px;
 				aspect-ratio: 1;
@@ -144,64 +198,75 @@ function App() {
 				</>}</For>
 			</div>
 		</main>
+		{css`
+			.bottomsheet-content {
+				display: grid;
+				grid-template-rows: 1fr auto;
+			}
+			.sidesheet-content {
+				display: grid;
+				grid-template-rows: auto 1fr auto;
+			}
+			.bottomsheet-content > :nth-child(1) { overflow-y: auto; }
+			.sidesheet-content   > :nth-child(2) { overflow-y: auto; }
+		`}
 		<Sheet sidesheet={sidesheet()} setSidesheet={setSidesheet}>
 			<NonResponsive>
-				<div class="[flex-shrink:0]">
+				<section>
 					{css`
-							.navbar {
-								padding: 16px;
-								/* Defer background-color and box-shadow to .*-card */
+						.navbar {
+							padding: 16px;
 
-								/* Flow */
-								display: flex;
-								flex-direction: row;
-								align-items: center; /* Center y-axis */
-								gap: 16px;
-							}
-						`}
+							/* Flow */
+							display: grid;
+							grid-template-columns: auto 1fr auto auto;
+							gap: 16px;
+						}
+						.navbar > :nth-child(1) { grid-column: 1; }
+						.navbar > :nth-child(2) { grid-column: 3; }
+						.navbar > :nth-child(3) { grid-column: 4; }
+					`}
 					<nav class="navbar">
 						<NavIcon />
-						<div class="[flex-grow:1]"></div>
 						<NavIcon />
 						<NavIcon />
 					</nav>
 					<div class="line"></div>
-				</div>
+				</section>
 			</NonResponsive>
-			<div class="[flex-grow:1] [overflow-y:auto]">
+			<section>
 				{css`
-						/* Use drawer-head-content because of line */
-						.drawer-head-content {
-							/* Use 24px for the x-axis padding because of <Radio> */
-							padding: 16px 24px;
+					/* Use drawer-head-content because of line and line.is-collapsed */
+					.drawer-head-content {
+						padding: 16px 24px; /* Add 8px to x-axis because of cp-checkable-label */
 
-							/* Flow */
-							display: flex;
-							flex-direction: row;
-							align-items: center;
-							gap: 8px;
-						}
-						.drawer-head-svg {
-							height: var(--form-svg-height);
-							aspect-ratio: 1;
-							color: gray;
-						}
-						.drawer-body {
-							padding: 16px;
-							padding-top: 0; /* Override */
+						/* Flow */
+						display: grid;
+						grid-template-columns: auto 1fr auto;
+						align-items: center;
+						gap: 8px;
+					}
+					.drawer-head-icon {
+						height: var(--form-svg-height);
+						aspect-ratio: 1;
+						color: gray;
+					}
+					.drawer-body {
+						padding: 16px;
+						padding-top: 0; /* Override */
 
-							/* Flow */
-							display: flex;
-							flex-direction: column;
-							gap: 8px;
-						}
-					`}
+						/* Flow */
+						display: flex;
+						flex-direction: column;
+						gap: 8px;
+					}
+				`}
 				<DrawerProvider>
 					{/* Omit <div class="line"> here */}
 					<Drawer head={<>
 						<div class="drawer-head-content">
-							<Dynamic component={SmileySVG} class="drawer-head-svg" />
-							<div class="[flex-grow:1]">Hello, world!</div>
+							<Dynamic component={SmileySVG} class="drawer-head-icon" />
+							<div>Hello, world!</div>
 							<div>Foo</div>
 						</div>
 					</>} open>
@@ -214,8 +279,8 @@ function App() {
 					<Drawer head={<>
 						<div class="line"></div>
 						<div class="drawer-head-content">
-							<Dynamic component={SmileySVG} class="drawer-head-svg" />
-							<div class="[flex-grow:1]">Hello, world!</div>
+							<Dynamic component={SmileySVG} class="drawer-head-icon" />
+							<div>Hello, world!</div>
 							<div>Foo</div>
 						</div>
 					</>} open>
@@ -228,8 +293,8 @@ function App() {
 					<Drawer head={<>
 						<div class="line"></div>
 						<div class="drawer-head-content">
-							<Dynamic component={SmileySVG} class="drawer-head-svg" />
-							<div class="[flex-grow:1]">Hello, world!</div>
+							<Dynamic component={SmileySVG} class="drawer-head-icon" />
+							<div>Hello, world!</div>
 							<div>Foo</div>
 						</div>
 					</>} open>
@@ -242,8 +307,8 @@ function App() {
 					<Drawer head={<>
 						<div class="line"></div>
 						<div class="drawer-head-content">
-							<Dynamic component={SmileySVG} class="drawer-head-svg" />
-							<div class="[flex-grow:1]">Hello, world!</div>
+							<Dynamic component={SmileySVG} class="drawer-head-icon" />
+							<div>Hello, world!</div>
 							<div>Foo</div>
 						</div>
 					</>} open>
@@ -252,8 +317,8 @@ function App() {
 					<Drawer head={<>
 						<div class="line"></div>
 						<div class="drawer-head-content">
-							<Dynamic component={SmileySVG} class="drawer-head-svg" />
-							<div class="[flex-grow:1]">Hello, world!</div>
+							<Dynamic component={SmileySVG} class="drawer-head-icon" />
+							<div>Hello, world!</div>
 							<div>Foo</div>
 						</div>
 					</>} open>
@@ -262,8 +327,8 @@ function App() {
 					<Drawer head={<>
 						<div class="line"></div>
 						<div class="drawer-head-content">
-							<Dynamic component={SmileySVG} class="drawer-head-svg" />
-							<div class="[flex-grow:1]">Hello, world!</div>
+							<Dynamic component={SmileySVG} class="drawer-head-icon" />
+							<div>Hello, world!</div>
 							<div>Foo</div>
 						</div>
 					</>} open>
@@ -271,8 +336,8 @@ function App() {
 					</Drawer>
 				</DrawerProvider>
 				<div class="line"></div>
-			</div>
-			<div class="[flex-shrink:0]">
+			</section>
+			<section>
 				<div class="line is-collapsed"></div>
 				<div class="[padding:16px] [display:flex] [flex-direction:row] [gap:16px]">
 					<div class="[height:80px] [aspect-ratio:16_/_9] [border-radius:8px] [background-color:gray]"></div>
@@ -288,165 +353,45 @@ function App() {
 					<div>This is the last block</div>
 					<div>This is the last block</div>
 				</div>
-			</div>
+			</section>
 		</Sheet>
 	</>
 }
 
-function App2() {
-	createScreenEffect()
-
-	const [checked, setChecked] = createSignal(false)
-	//// const [checked2, setChecked2] = createSignal(false)
-	//// const [checked3, setChecked3] = createSignal(false)
-
-	const [groupValue, setGroupValue] = createSignal("foo")
-
-	return <>
-		{css`
-			.foo-container {
-				--color:       hsl(200 100% 55%);
-				--alpha-color: hsl(200 100% 55% / 40%);
-
-				display: grid;
-				grid-template-columns: 1fr auto;
-				align-items: center; /* foo-container and foo-checkbox have different heights */
-				gap: 8px;
-
-				cursor: pointer;
-			}
-			.foo-label {
-				padding: 0 8px;
-				height: 32px;
-				border-radius: 1000px;
-				background-color: hsl(0 0% 95%);
-
-				/* Flow */
-				display: grid;
-				grid-template-columns: auto 1fr;
-				align-items: center;
-				gap: 8px;
-			}
-			.foo-label-icon {
-				height: 60%;
-				aspect-ratio: 1;
-				/* color: hsl(0 0% calc(100% / 3)); */
-				color: var(--color);
-			}
-			.foo-label-text {
-				font: 600 14px / normal system-ui;
-				letter-spacing: 0.025em;
-				color: hsl(0 0% calc(100% / 3));
-			}
-
-			/********************************/
-
-			.foo-checkbox {
-				height: 24px;
-				aspect-ratio: 1;
-				border-radius: calc(100% / 3);
-				/* Defer background-color and box-shadow */
-
-				/* Flow */
-				display: grid;
-				place-items: center;
-			}
-			.foo-container[aria-checked=false] .foo-checkbox-check { display: none; }
-			.foo-container[aria-checked=true]  .foo-checkbox-check {
-				height: 50%;
-				aspect-ratio: 1;
-				/* border-radius: 1000px; */
-				color: white;
-			}
-
-			/********************************/
-
-			.foo-radio {
-				height: 24px;
-				aspect-ratio: 1;
-				border-radius: 1000px;
-				/* Defer background-color and box-shadow */
-
-				/* Flow */
-				display: grid;
-				place-items: center;
-			}
-			.foo-container[aria-checked=false] .foo-radio-check { display: none; }
-			.foo-container[aria-checked=true]  .foo-radio-check {
-				height: calc(100% / 3);
-				aspect-ratio: 1;
-				border-radius: 1000px;
-				background-color: white;
-			}
-
-			/********************************/
-
-			.foo-container[aria-checked=false] :is(.foo-checkbox, .foo-radio) {
-				background-color: white;
-				box-shadow: 0 0 0 1px hsl(0 0% 0% / 10%),
-					0 0 0 4px hsl(0 0% 0% / 10%);
-			}
-			.foo-container[aria-checked=true] :is(.foo-checkbox, .foo-radio) {
-				background-color: var(--color);
-				box-shadow: /* 0 0 0 1px hsl(0 0% 0% / 10%), */
-					0 0 0 4px hsl(0 0% 0% / 10%);
-			}
-			.foo-container[aria-checked=false] :is(.foo-checkbox, .foo-radio):hover {
-				box-shadow: 0 0 0 1px hsl(0 0% 0% / 10%),
-					0 0 0 4px hsl(0 0% 0% / 10%),
-					0 0 0 8px hsl(0 0% 0% / 10%);
-			}
-			.foo-container[aria-checked=true] :is(.foo-checkbox, .foo-radio):hover {
-				box-shadow: /* 0 0 0 1px hsl(0 0% 0% / 10%), */
-					0 0 0 4px hsl(0 0% 0% / 10%),
-					0 0 0 8px var(--alpha-color);
-			}
-		`}
-		<div class="[display:grid] [place-items:center] [height:$screen-y]">
-			<div class="[width:448px] [display:flex] [flex-direction:column] [gap:8px]">
-				<AriaCheckbox class="foo-container" checked={checked()} setChecked={setChecked}>
-					<div class="foo-label">
-						<Dynamic component={SmileySVG} class="foo-label-icon" />
-						<div class="foo-label-text">FOO</div>
-					</div>
-					<div class="foo-checkbox">
-						<Dynamic component={CheckSVG} class="foo-checkbox-check" style={{ "stroke-width": "7" }} />
-					</div>
-				</AriaCheckbox>
-				<AriaRadiogroup class="[display:grid] [grid-template-columns:1fr_2px_1fr_2px_1fr] [gap:16px]" groupValue={groupValue()} setGroupValue={setGroupValue}>
-					<For each={[
-						{ style: { "--color": "#ffb13b", "--alpha-color": "#ffb13b66" }, value: "foo" },
-						{ style: { "--color": "#61dafb", "--alpha-color": "#61dafb66" }, value: "bar" },
-						{ style: { "--color": "#4fc08d", "--alpha-color": "#4fc08d66" }, value: "baz" },
-					]}>{({ style, value }, index) => <>
-						<Show when={index() > 0}>
-							<div class="[margin:8px_0] [border-radius:1000px] [background-color:hsl(0_0%_90%)]"></div>
-						</Show>
-						<AriaRadio class="foo-container" style={style} value={value}>
-							<div class="foo-label">
-								<Dynamic component={SmileySVG} class="foo-label-icon" />
-								<div class="foo-label-text">FOO</div>
-							</div>
-							<div class="foo-radio">
-								<div class="foo-radio-check"></div>
-							</div>
-						</AriaRadio>
-					</>}</For>
-				</AriaRadiogroup>
-			</div>
-		</div>
-	</>
-}
-
-function CheckSVG(props: VoidProps<RefProps & CSSProps>) {
-	return <>
-		<svg ref={el => props.ref?.(el as unknown as HTMLElement)} class={props.class} style={props.style} fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-			<polyline points="20 6 9 17 4 12"></polyline>
-		</svg>
-	</>
-}
+//// function App2() {
+//// 	createScreenEffect()
+////
+//// 	const [checked, setChecked] = createSignal(false)
+//// 	const [groupValue, setGroupValue] = createSignal("foo")
+////
+//// 	return <>
+//// 		<div class="[display:grid] [place-items:center] [height:$screen-y]">
+//// 			<div class="[width:416px] [display:flex] [flex-direction:column] [gap:8px]">
+//// 				{/* Checkboxes */}
+//// 				<CheckableCheckbox checked={checked()} setChecked={setChecked}>
+//// 					FOO
+//// 				</CheckableCheckbox>
+//// 				{/* Radios */}
+//// 				<AriaRadiogroup class="[display:grid] [grid-template-columns:1fr_2px_1fr_2px_1fr] [gap:16px]" groupValue={groupValue()} setGroupValue={setGroupValue}>
+//// 					<For each={[
+//// 						{ style: { "--color": "#ffb13b", "--alpha-color": "#ffb13b66" }, value: "foo" },
+//// 						{ style: { "--color": "#61dafb", "--alpha-color": "#61dafb66" }, value: "bar" },
+//// 						{ style: { "--color": "#4fc08d", "--alpha-color": "#4fc08d66" }, value: "baz" },
+//// 					]}>{(p, index) => <>
+//// 						<Show when={index() > 0}>
+//// 							<div class="[margin:8px_0] [border-radius:1000px] [background-color:hsl(0_0%_90%)]"></div>
+//// 						</Show>
+//// 						<CheckableRadio style={p.style} value={p.value}>
+//// 							{p.value.toUpperCase()}
+//// 						</CheckableRadio>
+//// 					</>}</For>
+//// 				</AriaRadiogroup>
+//// 			</div>
+//// 		</div>
+//// 	</>
+//// }
 
 render(() =>
-	<App2 />,
+	<App />,
 	document.getElementById("root")!,
 )
