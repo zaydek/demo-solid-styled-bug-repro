ab70ec4053db3bf21c2f805ccc5abf28066b3687
Zaydek Michels-Gualtieri <zaydekdotcom@gmail.com>
2022-10-29 16:21:02 -0700

    Remove dead code

--- src/utils/vanilla/stringify.ts
+++
@@ -1,9 +0,0 @@
-export function stringify(state: any, space?: number | string) {
-	return JSON.stringify(state, (_, v) => {
-		if (typeof v === "function") {
-			return v()
-		} else {
-			return v
-		}
-	}, space)
-}
